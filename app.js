a03b02324c155f6412e036cbba111e8b9f44b574
alvaromrtn <alvaromrtn@gmail.com>
2022-03-09 19:10:18 +0100

    login

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const hbs = require("hbs");
 const app = express();
 const path = require("path");
 
+const bodyParser = require('body-parser'); // middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,5 +19,21 @@ app.get("/random", (req, res, next) => res.render("random"));
 app.get("/buscar", (req, res, next) => res.render("buscar"));
 
 app.get("/contacto", (req, res, next) => res.render("contacto"));
+
+
+app.get('/login', (req, res, next) => res.render("login"));
+
+app.post('/login', (req, res) => {
+    // Insert Login Code Here
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if ((username == "admin") && (password == "admin")) {
+        res.send("<div align='center'><h2>USUARIO Y CONTRASEÑA CORRECTOS</h2></div>");
+    } else {
+        res.send("<div align='center'><h2>ERROR</h2></div>");
+    }
+});
+
 
 app.listen(3000);
