96a8b14ba6e8140404f6e02ca03a57b3dfd7a7d5
dmm1005 <dmm1005@alu.ubu.es>
2022-03-09 20:57:47 +0100

    Reestructurado layout pero ha fallado la redimension. Revisar

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//referencia: https://github.com/Jossdz/lab-spotify-express
+
+
+
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
@@ -13,8 +17,44 @@ app.get("/", (req, res, next) => res.render("home"));
 
 app.get("/random", (req, res, next) => res.render("random"));
 
-app.get("/buscar", (req, res, next) => res.render("buscar"));
+//app.get("/:buscar", (req, res, next) => res.render("buscar"));
+
+app.get("/buscar/:query", (req, res) => {
+    res.render("buscar");
+    res.send(req.params);
+});
 
 app.get("/contacto", (req, res, next) => res.render("contacto"));
+
+// app.route('/logon')
+//     .get(sessionChecker, (req, res) => {
+//         //res.sendFile(__dirname + '/public/login.html');
+//         res.render("logon");
+//     })
+//     .post((req, res) => {
+//         var username = req.body.username,
+//             password = req.body.password;
+
+//         User.findOne({ where: { username: username } }).then(function (user) {
+//             if (!user) {
+//                 res.redirect('/logon');
+//             } else if (!user.validPassword(password)) {
+//                 res.redirect('/logon');
+//             } else {
+//                 req.session.user = user.dataValues;
+//                 res.redirect('/home');
+//             }
+//         });
+//     });
+
+
+
+app.get('/logon', (req, res, next) => res.render("logon"));
+
+
+//app.post('/logon', (req, res) => res.send("Logon submitted"));
+
+
+
 
 app.listen(3000);
